60522056c517430f8cfb0e6a67b1721d0c7ec544
Gavin Stone <gavsto@gmail.com>
2020-05-06 15:05:22 +0100

    Create Symantec Removal.ps1

---
+++ Symantec Removal.ps1
@@ -0,0 +1,17 @@
+$wshell = New-Object -ComObject wscript.shell
+Start-Process -FilePath "C:\SEPprep\SepPrep64.exe"
+Sleep 10
+$wshell.AppActivate('Symantec Endpoint Protection Cloud')
+#$wshell.AppActivate('Untitled - notepad')
+Sleep 2
+$wshell.SendKeys('{TAB}')
+Sleep 0.5
+$wshell.SendKeys('{TAB}')
+Sleep 0.5
+$wshell.SendKeys('{ENTER}')
+Sleep 60
+$wshell.AppActivate('Symantec Endpoint Protection Cloud')
+Sleep 0.5
+$wshell.SendKeys('{TAB}')
+Sleep 0.5
+$wshell.SendKeys('{ENTER}')
